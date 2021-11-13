bc087d02babcfd2cafe5e10746735bcc324f1921
Raghav <raghav.gulati@gmail.com>
2021-11-13 21:58:20 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+// components
+ 
+import {PageNotFoundComponent} from './shared/not-found/not-found.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/games', pathMatch: 'full'},
+  {
+    path: 'games',
+    loadChildren:() => import(`./games/games.module`).then(
+      module => module.GamesModule)
+  },
+  {path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
